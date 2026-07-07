5fd4b1e012d797ce5d27210adb3fadc4b6d59f9c
edik06031 <edik06031@gmail.com>
2026-07-07 15:18:33 +0200

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -32,3 +32,46 @@ JOIN olist_order_items_dataset oi USING (order_id)
 WHERE o.order_status = 'delivered'
 GROUP BY ym
 ORDER BY ym;
+
+-- розвідувальні запити
+-- топ 10 категорій за виторгом
+SELECT
+	t.product_category_1 AS category_en,
+    round(sum(oi.price), 2) AS revenue
+
+FROM olist_order_items_dataset oi
+join olist_orders_dataset o USING (order_id)
+JOIN olist_products_dataset p USING(product_id)
+LEFT JOIN product_category_name_translation t USING (product_category)
+where o.order_status = 'delivered'
+GROUP By category_en
+ORDER by revenue DESC
+LIMIT 10;
+
+-- виторг за штатами (для карти в Tableau)
+SELECT
+	cu.customer_state,
+    ROUND(SUM(oi.price), 2) AS revenue,
+    COUNT(DISTINCT o.order_id) AS orders
+FROM olist_order_items_dataset oi
+JOIN olist_orders_dataset o USING (order_id)
+JOIN olist_customers_dataset cu USING (customer_id)
+WHERE o.order_status = 'delivered'
+GROUP BY cu.customer_state
+ORDER BY revenue DESC;
+
+
+--      середня оцінка (review_score) за категоріями
+
+SELECT
+	t.product_category_1 AS category_en,
+    round(avg(r.review_score), 2) as avg_score,
+    COUNT(*) AS reviews
+
+FROM olist_order_reviews_dataset r 
+JOIN olist_order_items_dataset oi USING (order_id)
+JOIN olist_products_dataset p USING (product_id)
+LEFT JOIN product_category_name_translation t USING (product_category)
+GROUP BY category_en
+HAVING reviews > 50
+ORDER BY avg_score DESC;
